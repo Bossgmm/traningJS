7a235afc6f35bdd8d597a9c39b8e97e020b57ded
Murad <goysultanov.murad@gmail.com>
2026-04-23 21:47:40 +0300

    Уровень 1.1

---
+++ script.js
@@ -0,0 +1,36 @@
+//Уровень 1.1
+//№1
+// let a = prompt();
+
+// if(a>0)
+//     console.log("a>0");
+// else
+//     console.log("a<0");
+
+//№2
+// let str = prompt();
+
+// console.log("Длина строки " + str.length);
+
+//№3
+// let str = prompt();
+
+// console.log(str.at(-1));
+
+//№4
+// let numb = prompt();
+
+// if(numb % 2 === 0)
+//     console.log("chet");
+// else
+//     console.log("nechet");
+
+//№5
+// let str1 = prompt();
+// let str2 = prompt();
+
+// if(str1.at() == str2.at())
+//     console.log('OK');
+// else
+//     console.log('NEOK');
+
